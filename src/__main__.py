2f15c731a2af0cc16c50f7d8f614357aced8ecd0
Henry Hay-Smith <hha100@uclive.ac.nz>
2021-05-26 16:01:42 +1200

    Initial commit

---
+++ src/__main__.py
@@ -0,0 +1,19 @@
+"""
+COSC364 Assignment 2
+Flow Planning
+
+Authors: Henry Hay-Smith and Rory Patterson
+Date: 26/05/2021
+
+__main__.py is run by default when naming the directory in the command line, meaning you don't need to specify
+which file you want to run
+e.g. 'py src' """
+
+
+def main():
+    """ Starts the program and runs other python scripts """
+    print()
+
+# Runs the main function automatically
+if __name__ == "__main__":
+    main()
