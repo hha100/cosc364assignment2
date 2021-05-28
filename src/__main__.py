8bbdd7bc9ca8f896daa7af181106c6878b31ce66
Henry Hay-Smith <hha100@uclive.ac.nz>
2021-05-28 15:52:01 +1200

    Added class for LP file storage and string representation

--- src/__main__.py
+++ src/__main__.py
@@ -12,6 +12,7 @@ e.g. 'py src'
 
 
 import sys
+import time
 
 
 class Config:
@@ -27,36 +28,53 @@ class Config:
     def inputs(self):
         """ Reads and parses the users' inputs to get the X, Y, and Z values """
         done = 0
-        
         while not done:
-            raw_input = input(f'\nEnter three positive integers separated by a space:\n')
-            
+            raw_input = input(f'Enter three positive integers separated by a space:\n')
             for char in raw_input:
-                
                 if char != ' ' and not char.isnumeric():
                     print(f'\nInvalid characters found in the input.\n')
                     break
-                
             vals = raw_input.split(' ')
-            
             if len(vals) != 3:
-                print(f'Exactly 3 positive integers, separated by a single space, are required.\n')
-                
+                print(f'\nExactly 3 positive integers, separated by a single space, are required.\n')
             elif int(vals[1]) < 2:
-                print(f'Y must be greater than or equal to 2.\n')
-                
+                print(f'\nY must be greater than or equal to 2.\n')
             else:
                 X, Y, Z = int(vals[0]), int(vals[1]), int(vals[2])
                 done = 1
-                
         return X, Y, Z
 
+    def set_LP(self, LP):
+        """ Sets the LP file for the user's input """
+        self.LP = LP
+
+class LP_File:
+    
+    def __init__(self):
+        """ Initialises the variable holding the list of lines in the generated LP file """
+        self.LP = []
+    
+    def __repr__(self):
+        """ Returns a string representation of the generated LP file """
+        return '\n'.join(self.LP)
 
 def main():
     """ Starts the program and runs support scripts """
     config = Config()
-    print(f'\n{config}')
-
+    print(f'\nInputs Accepted:\n    {config}\n')
+    
+    # Call LP file generation function here
+    LP = LP_File()
+    config.set_LP(LP)
+    print(f'LP file is:\n{config.LP if config.LP.LP else "    EMPTY LP FILE"}')
+    
+    start_time = time.time()
+    CPLEX_done = 0 # Debug variable
+    
+    # Call CPLEX here
+    
+    end_time = f'{time.time() - start_time:.4f}'
+    print(f'\nCPLEX Execution Time is:\n    {end_time if CPLEX_done else 0} s\n')
 
 if __name__ == "__main__":
     """ Runs the main function automatically """
