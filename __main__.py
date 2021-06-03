66c6bc2885a1e23169faa14e99745edcc124aee5
Henry Hay-Smith <hha100@uclive.ac.nz>
2021-06-04 11:43:37 +1200

    '.'

--- __main__.py
+++ __main__.py
@@ -8,6 +8,7 @@ Date: 26/05/2021
 __main__.py is run by default when naming the directory in the command line, meaning you don't need to specify
 which file you want to run
 e.g. 'py src'
+
 """
 
 
@@ -36,9 +37,9 @@ class Config:
                     break
             vals = raw_input.split(' ')
             if len(vals) != 3:
-                print(f'\nExactly 3 positive integers, separated by a single space, are required.\n')
+                print(f'Exactly 3 positive integers, separated by a single space, are required.\n')
             elif int(vals[1]) < 2:
-                print(f'\nY must be greater than or equal to 2.\n')
+                print(f'Y must be greater than or equal to 2.\n')
             else:
                 X, Y, Z = int(vals[0]), int(vals[1]), int(vals[2])
                 done = 1
@@ -47,6 +48,29 @@ class Config:
     def set_LP(self, LP):
         """ Sets the LP file for the user's input """
         self.LP = LP
+    
+    def demands(self):
+        """ Creates a table of demand volumes """
+        self.demands = []
+        for i in range(self.X):
+            self.demands.append([i+j+2 for j in range(self.Z)])
+        
+        print(f'Demand Volume Table:')
+        for line in self.demands:
+            print(line)
+        print()
+    
+    def capacities(self):
+        """ Creates arbitrary tables of link capacitances """
+        self.Cik = []
+        self.Dkj = []
+        
+        for i in range(self.X):
+            self.Cik.append([10 for k in range(self.Y)])
+        
+        for k in range(self.Y):
+            self.Dkj.append([10 for j in range(self.Z)])
+    
 
 class LP_File:
     
@@ -63,6 +87,12 @@ def main():
     config = Config()
     print(f'\nInputs Accepted:\n    {config}\n')
     
+    # Generate a table of demand volumes
+    config.demands()
+    
+    # Generate an arbirary list of link capacities
+    config.capacities()
+    
     # Call LP file generation function here
     LP = LP_File()
     config.set_LP(LP)
@@ -73,9 +103,10 @@ def main():
     
     # Call CPLEX here
     
+    #CPLEX_done = 1
+    
+    # Find the CPLEX execution time
     end_time = f'{time.time() - start_time:.4f}'
-    print(f'\nCPLEX Execution Time is:\n    {end_time if CPLEX_done else 0} s\n')
+    print(f'\nCPLEX Execution Time is:\n    {end_time+" s" if CPLEX_done else "CPLEX not performed"}\n')
 
-if __name__ == "__main__":
-    """ Runs the main function automatically """
-    main()
+main()
